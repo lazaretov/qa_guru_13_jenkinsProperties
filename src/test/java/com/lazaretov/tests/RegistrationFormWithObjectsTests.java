4a2d5bef17ad3ae95f5cff802201a45c68c7a16a
Timofei <lazaretov03@gmail.com>
2023-01-13 11:46:52 +0300

    add TestBase and Configurations

--- src/test/java/com/lazaretov/tests/RegistrationFormWithObjectsTests.java
+++ src/test/java/com/lazaretov/tests/RegistrationFormWithObjectsTests.java
@@ -1,47 +1,15 @@
 package com.lazaretov.tests;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import com.lazaretov.helpers.Attach;
 import com.lazaretov.pages.RegistrationFormPage;
 import io.qameta.allure.*;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.lazaretov.testData.UserInfo.*;
 import static io.qameta.allure.Allure.step;
 
-public class RegistrationFormWithObjectsTests {
+public class RegistrationFormWithObjectsTests extends TestBase{
     RegistrationFormPage registrationFormPage = new RegistrationFormPage();
-
-    @BeforeAll
-    static void setUp() {
-        SelenideLogger.addListener("allure", new AllureSelenide());
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-            // capabilities.setCapability("browserName", "chrome");
-            // capabilities.setCapability("browserVersion", "100.0");
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-
-        Configuration.browserCapabilities = capabilities;
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browserSize = "1920x1080";
-            // Configuration.holdBrowserOpen = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-    }
-
-    @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-    }
 
     @Test
     @Feature("Форма регистрации")
@@ -51,7 +19,7 @@ public class RegistrationFormWithObjectsTests {
     @Link(value = "Testing site", url = "https://demoqa.com")
     @DisplayName("Проверка полей формы регистрации")
     void fillFormTest() {
-        step("Открыть и заполинть форму", () -> {
+        step("Открыть и заполнить форму", () -> {
             registrationFormPage.openPage()
                     .setFirstName(firstName)
                     .setLastName(lastName)
